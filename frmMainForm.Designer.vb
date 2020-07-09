2c4f856f8a6dced0d3c94ce045bf9ea4db7933b3
THOMAS KLUGE <THOKLU2@de.ibm.com>
2020-07-09 15:36:15 +0200

    Initial Commit

---
+++ frmMainForm.Designer.vb
@@ -0,0 +1,174 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMainForm
+  Inherits System.Windows.Forms.Form
+
+  'Form overrides dispose to clean up the component list.
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+    Try
+      If disposing AndAlso components IsNot Nothing Then
+        components.Dispose()
+      End If
+    Finally
+      MyBase.Dispose(disposing)
+    End Try
+  End Sub
+
+  'Required by the Windows Form Designer
+  Private components As System.ComponentModel.IContainer
+
+  'NOTE: The following procedure is required by the Windows Form Designer
+  'It can be modified using the Windows Form Designer.  
+  'Do not modify it using the code editor.
+  <System.Diagnostics.DebuggerStepThrough()> _
+  Private Sub InitializeComponent()
+    Me.txtOutputString = New System.Windows.Forms.TextBox()
+    Me.Label1 = New System.Windows.Forms.Label()
+    Me.Label2 = New System.Windows.Forms.Label()
+    Me.cmdConvert = New System.Windows.Forms.Button()
+    Me.Label3 = New System.Windows.Forms.Label()
+    Me.Label4 = New System.Windows.Forms.Label()
+    Me.chkErrorOnEmpty = New System.Windows.Forms.CheckBox()
+    Me.cboDecimalSeparator = New System.Windows.Forms.ComboBox()
+    Me.txtInputString = New System.Windows.Forms.TextBox()
+    Me.SuspendLayout()
+    '
+    'txtOutputString
+    '
+    Me.txtOutputString.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+    Me.txtOutputString.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+    Me.txtOutputString.Location = New System.Drawing.Point(12, 366)
+    Me.txtOutputString.Multiline = True
+    Me.txtOutputString.Name = "txtOutputString"
+    Me.txtOutputString.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+    Me.txtOutputString.Size = New System.Drawing.Size(794, 343)
+    Me.txtOutputString.TabIndex = 1
+    Me.txtOutputString.WordWrap = False
+    '
+    'Label1
+    '
+    Me.Label1.AutoSize = True
+    Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+    Me.Label1.Location = New System.Drawing.Point(16, 12)
+    Me.Label1.Name = "Label1"
+    Me.Label1.Size = New System.Drawing.Size(72, 15)
+    Me.Label1.TabIndex = 2
+    Me.Label1.Text = "Input String:"
+    '
+    'Label2
+    '
+    Me.Label2.AutoSize = True
+    Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+    Me.Label2.Location = New System.Drawing.Point(16, 349)
+    Me.Label2.Name = "Label2"
+    Me.Label2.Size = New System.Drawing.Size(46, 15)
+    Me.Label2.TabIndex = 3
+    Me.Label2.Text = "Output:"
+    '
+    'cmdConvert
+    '
+    Me.cmdConvert.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+    Me.cmdConvert.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+    Me.cmdConvert.Location = New System.Drawing.Point(730, 8)
+    Me.cmdConvert.Name = "cmdConvert"
+    Me.cmdConvert.Size = New System.Drawing.Size(75, 24)
+    Me.cmdConvert.TabIndex = 4
+    Me.cmdConvert.Text = "Convert"
+    Me.cmdConvert.UseVisualStyleBackColor = True
+    '
+    'Label3
+    '
+    Me.Label3.AutoSize = True
+    Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+    Me.Label3.Location = New System.Drawing.Point(105, 12)
+    Me.Label3.Name = "Label3"
+    Me.Label3.Size = New System.Drawing.Size(113, 15)
+    Me.Label3.TabIndex = 6
+    Me.Label3.Text = "Decimal Separator:"
+    '
+    'Label4
+    '
+    Me.Label4.AutoSize = True
+    Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+    Me.Label4.Location = New System.Drawing.Point(308, 12)
+    Me.Label4.Name = "Label4"
+    Me.Label4.Size = New System.Drawing.Size(164, 15)
+    Me.Label4.TabIndex = 7
+    Me.Label4.Text = "A=Auto; R=Regional Settings"
+    '
+    'chkErrorOnEmpty
+    '
+    Me.chkErrorOnEmpty.AutoSize = True
+    Me.chkErrorOnEmpty.Checked = Global.WindowsApp1.My.MySettings.Default.ErrorOnEmpty
+    Me.chkErrorOnEmpty.DataBindings.Add(New System.Windows.Forms.Binding("Checked", Global.WindowsApp1.My.MySettings.Default, "ErrorOnEmpty", True, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged))
+    Me.chkErrorOnEmpty.Location = New System.Drawing.Point(489, 14)
+    Me.chkErrorOnEmpty.Name = "chkErrorOnEmpty"
+    Me.chkErrorOnEmpty.Size = New System.Drawing.Size(149, 17)
+    Me.chkErrorOnEmpty.TabIndex = 8
+    Me.chkErrorOnEmpty.Text = "Error on empty input value"
+    Me.chkErrorOnEmpty.UseVisualStyleBackColor = True
+    '
+    'cboDecimalSeparator
+    '
+    Me.cboDecimalSeparator.DataBindings.Add(New System.Windows.Forms.Binding("Text", Global.WindowsApp1.My.MySettings.Default, "DecimalSeparator", True, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged))
+    Me.cboDecimalSeparator.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+    Me.cboDecimalSeparator.Font = New System.Drawing.Font("Courier New", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+    Me.cboDecimalSeparator.FormattingEnabled = True
+    Me.cboDecimalSeparator.Items.AddRange(New Object() {"A", "R", ",", "."})
+    Me.cboDecimalSeparator.Location = New System.Drawing.Point(222, 8)
+    Me.cboDecimalSeparator.Name = "cboDecimalSeparator"
+    Me.cboDecimalSeparator.Size = New System.Drawing.Size(78, 23)
+    Me.cboDecimalSeparator.TabIndex = 5
+    Me.cboDecimalSeparator.Text = Global.WindowsApp1.My.MySettings.Default.DecimalSeparator
+    '
+    'txtInputString
+    '
+    Me.txtInputString.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+    Me.txtInputString.DataBindings.Add(New System.Windows.Forms.Binding("Text", Global.WindowsApp1.My.MySettings.Default, "InputString", True, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged))
+    Me.txtInputString.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+    Me.txtInputString.Location = New System.Drawing.Point(12, 36)
+    Me.txtInputString.Multiline = True
+    Me.txtInputString.Name = "txtInputString"
+    Me.txtInputString.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+    Me.txtInputString.Size = New System.Drawing.Size(794, 301)
+    Me.txtInputString.TabIndex = 0
+    Me.txtInputString.Text = Global.WindowsApp1.My.MySettings.Default.InputString
+    Me.txtInputString.WordWrap = False
+    '
+    'frmMainForm
+    '
+    Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+    Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+    Me.ClientSize = New System.Drawing.Size(820, 720)
+    Me.Controls.Add(Me.chkErrorOnEmpty)
+    Me.Controls.Add(Me.Label4)
+    Me.Controls.Add(Me.Label3)
+    Me.Controls.Add(Me.cboDecimalSeparator)
+    Me.Controls.Add(Me.cmdConvert)
+    Me.Controls.Add(Me.Label2)
+    Me.Controls.Add(Me.Label1)
+    Me.Controls.Add(Me.txtOutputString)
+    Me.Controls.Add(Me.txtInputString)
+    Me.MaximizeBox = False
+    Me.MinimizeBox = False
+    Me.Name = "frmMainForm"
+    Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+    Me.Text = "String to Double Convertierung"
+    Me.ResumeLayout(False)
+    Me.PerformLayout()
+
+  End Sub
+
+  Friend WithEvents txtInputString As TextBox
+  Friend WithEvents txtOutputString As TextBox
+  Friend WithEvents Label1 As Label
+  Friend WithEvents Label2 As Label
+  Friend WithEvents cmdConvert As Button
+  Friend WithEvents cboDecimalSeparator As ComboBox
+  Friend WithEvents Label3 As Label
+  Friend WithEvents Label4 As Label
+  Friend WithEvents chkErrorOnEmpty As CheckBox
+End Class
---
+++ frmMainForm.vb
@@ -0,0 +1,193 @@
+﻿Public Class frmMainForm
+  Private Sub frmMainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+  End Sub
+
+  Private Sub frmMainForm_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+    txtInputString.SelectionStart = 0
+    txtInputString.SelectionLength = 0
+
+  End Sub
+
+  Private Sub cmdConvert_Click(sender As Object, e As EventArgs) Handles cmdConvert.Click
+    Dim dblResult As Decimal
+    Dim sResult As String
+    Dim sDecimalSep As String = String.Empty, sLine As String
+
+    If cboDecimalSeparator.SelectedIndex = 0 Then
+      sDecimalSep = "A"
+    ElseIf cboDecimalSeparator.SelectedIndex = 1 Then
+      sDecimalSep = "R"
+    ElseIf cboDecimalSeparator.SelectedIndex > 1 Then
+      sDecimalSep = cboDecimalSeparator.Text
+    End If
+
+    txtOutputString.Text = String.Empty
+
+    '*** Check Nothing working
+    sLine = Nothing
+    Try
+      dblResult = ParseDouble(sLine, sDecimalSep, chkErrorOnEmpty.Checked)
+      sResult = String.Format("{0:0.00000}", dblResult)
+    Catch ex As Exception
+      sResult = ex.Message
+    End Try
+    txtOutputString.Text += String.Format("{0,14} ==> {1,17}", "Nothing", sResult) + vbCrLf
+
+    '*** Loop input values
+    For Each sLine In txtInputString.Lines
+      Try
+        dblResult = ParseDouble(sLine, sDecimalSep, chkErrorOnEmpty.Checked)
+        sResult = String.Format("{0:0.00000}", dblResult)
+      Catch ex As Exception
+        sResult = ex.Message
+      End Try
+
+      txtOutputString.Text += String.Format("{0,14} ==> {1,17}", sLine, sResult) + vbCrLf
+    Next
+  End Sub
+
+
+
+  ''' <summary>
+  ''' Parse double value string into double variable
+  ''' </summary>
+  ''' <param name="oValue">Input Value</param>
+  ''' <param name="sDecimalSeparator">Use given Decimal Separator
+  '''                                 'A' or Empty ==> Automatic determination
+  '''                                 'R'          ==> Use Regional Settings
+  '''                                 ','          ==> Use komma as decimal separator
+  '''                                 '.'          ==> Use dot as decimal separator</param>
+  ''' <param name="bRaiseErrorOnEmpty">Raise error on empty value</param>
+  ''' <returns>Double value</returns>
+  ''' <remarks>Created KT 2020-07-09</remarks>
+  Private Shared Function ParseDouble(ByVal oValue As Object, Optional sDecimalSeparator As String = "A", Optional bRaiseErrorOnEmpty As Boolean = False) As Double
+    Dim dblResult As Double = Nothing
+    Dim sDoubleAsString As String = oValue
+    Dim oDoubleAsCharList As IEnumerable(Of Char)
+    Dim oNfi As System.IFormatProvider
+    Const ERROR_TEXT As String = "Error parsing '{0}'."
+
+    '*** Check Nothing given
+    If (oValue Is Nothing) Then
+      If bRaiseErrorOnEmpty Then
+        Throw New Exception(String.Format(ERROR_TEXT + " No value given!", oValue))
+      Else
+        Return 0.0
+      End If
+    End If
+
+    '*** String correction
+    oDoubleAsCharList = sDoubleAsString.ToList()
+    sDoubleAsString = sDoubleAsString.Trim
+    sDoubleAsString = sDoubleAsString.Replace(" ", "")
+
+    '*** Check Value given
+    If (sDoubleAsString.Length = 0) Then
+      If bRaiseErrorOnEmpty Then
+        Throw New Exception(String.Format(ERROR_TEXT + " No value given!", oValue))
+      Else
+        Return 0.0
+      End If
+    End If
+
+    '*** Check input value has numbers
+    If Not HasNumbers(sDoubleAsString) Then
+      Throw New Exception(String.Format(ERROR_TEXT + " No numeric value found!", oValue))
+    End If
+
+    '*** String correction
+    sDoubleAsString = RemoveNonNumberDigitsAndCharacters(sDoubleAsString)
+
+    '*** Use given decimal separator
+    If (sDecimalSeparator = "A") OrElse (sDecimalSeparator = "") Then
+      oNfi = System.Globalization.CultureInfo.InvariantCulture
+    ElseIf sDecimalSeparator = "R" Then
+      oNfi = System.Globalization.CultureInfo.CurrentCulture
+    ElseIf (sDecimalSeparator = ",") OrElse (sDecimalSeparator = ".") Then
+      oNfi = New Globalization.NumberFormatInfo With {.CurrencyDecimalSeparator = sDecimalSeparator}
+    Else
+      sDecimalSeparator = String.Empty
+      oNfi = System.Globalization.CultureInfo.InvariantCulture
+    End If
+
+    '*** Remove thousend separators
+    If sDecimalSeparator = "," Then
+      sDoubleAsString = sDoubleAsString.Replace(".", "")
+      If oDoubleAsCharList.Where(Function(ch) ch = ",").Count() > 1 Then
+        Throw New Exception(String.Format(ERROR_TEXT + " Try removing doublicate separators", oValue))
+      End If
+
+    ElseIf sDecimalSeparator = "." Then
+      sDoubleAsString = sDoubleAsString.Replace(",", "")
+      If oDoubleAsCharList.Where(Function(ch) ch = ".").Count() > 1 Then
+        Throw New Exception(String.Format(ERROR_TEXT + " Try removing doublicate separators", oValue))
+      End If
+    End If
+
+    '*** Parse using regional settings
+    If sDecimalSeparator = "R" Then
+      Try
+        dblResult = Double.Parse(sDoubleAsString, System.Globalization.NumberStyles.Any, oNfi)
+      Catch ex As Exception
+        Throw New Exception(String.Format(ERROR_TEXT + " " + ex.Message, oValue))
+      End Try
+
+      '*** Auto parse
+    ElseIf oDoubleAsCharList.Where(Function(ch) ch = "." OrElse ch = ",").Count() <= 1 Then
+      Try
+        dblResult = Double.Parse(sDoubleAsString.Replace(",", "."), System.Globalization.NumberStyles.Any, oNfi)
+      Catch ex As Exception
+        Throw New Exception(String.Format(ERROR_TEXT + " " + ex.Message, oValue))
+      End Try
+
+    Else
+      '*** Auto parse
+      If oDoubleAsCharList.Where(Function(ch) ch = ".").Count() >= 1 AndAlso oDoubleAsCharList.Where(Function(ch) ch = ",").Count() >= 1 Then
+        If sDoubleAsString.IndexOf(".") < sDoubleAsString.IndexOf(",") Then
+          sDoubleAsString = sDoubleAsString.Replace(".", String.Empty)
+        Else
+          sDoubleAsString = sDoubleAsString.Replace(",", String.Empty)
+        End If
+        Double.TryParse(sDoubleAsString.Replace(","c, "."), System.Globalization.NumberStyles.Any, oNfi, dblResult)
+
+      ElseIf oDoubleAsCharList.Where(Function(ch) ch = ".").Count() <= 1 AndAlso oDoubleAsCharList.Where(Function(ch) ch = ",").Count() > 1 Then
+        Double.TryParse(sDoubleAsString.Replace(",", String.Empty), System.Globalization.NumberStyles.Any, oNfi, dblResult)
+
+      ElseIf oDoubleAsCharList.Where(Function(ch) ch = ",").Count() <= 1 AndAlso oDoubleAsCharList.Where(Function(ch) ch = ".").Count() > 1 Then
+        Double.TryParse(sDoubleAsString.Replace(".", String.Empty).Replace(",", "."), System.Globalization.NumberStyles.Any, oNfi, dblResult)
+
+      Else
+        Throw New Exception(String.Format(ERROR_TEXT + " Try removing thousand separators.", oValue))
+      End If
+
+    End If
+
+    Return dblResult
+  End Function
+
+  ''' <summary>
+  ''' Remove none numeric characters for parsing input string
+  ''' </summary>
+  ''' <param name="sInputText">Input string</param>
+  ''' <returns>Corrected string</returns>
+  ''' <remarks>Created KT 2020-07-09</remarks>
+  Private Shared Function RemoveNonNumberDigitsAndCharacters(ByVal sInputText As String) As String
+    Dim oNumericChars = "0123456789,.-+e".ToCharArray()
+    Return New String(sInputText.Where(Function(c) oNumericChars.Any(Function(n) n = c)).ToArray())
+  End Function
+
+  ''' <summary>
+  ''' Check input value contains numbers
+  ''' </summary>
+  ''' <param name="sInputText">Input string</param>
+  ''' <returns>True, input value contains at least one number</returns>
+  ''' <remarks>Created KT 2020-07-09</remarks>
+  Private Shared Function HasNumbers(ByVal sInputText As String) As Boolean
+    Dim oNumericChars = "0123456789".ToCharArray()
+    Return New String(sInputText.Where(Function(c) oNumericChars.Any(Function(n) n = c)).ToArray()).Length > 0
+  End Function
+
+
+End Class
